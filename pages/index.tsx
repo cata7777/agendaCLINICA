e964d3e57467fc9bfeeb5790df9754f906cf10e2
cata7777 <CATALINAROJOLEMA@GMAIL.COM>
2025-05-04 12:47:28 -0400

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Index() {
+  return (
+    <div>
+      <h1>Bienvenid@ a Agenda Clínica</h1>
+      <p>Esta es una versión de inicio compatible con Vercel.</p>
+    </div>
+  );
+}
